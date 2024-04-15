9d22f9fd3b8367df6984f95c1d9e40f3c53857b5
fangdaoyou <471727250@qq.com>
2024-04-15 13:17:49 +0800

    init

--- run_code/plgmot_fsc.sh
+++
@@ -1,9 +0,0 @@
-python train.py --config configs/plgmot_fsc.py --work-dir ./work_dirs/plgmot_fsc1
-python detect.py --exp_id plgmot_fsc1 --config work_dirs/plgmot_fsc1/plgmot_fsc.py --checkpoint work_dirs/plgmot_fsc1/latest.pth
-python hmaa_track.py --exp_id plgmot_fsc1
-python sort.py --exp_id plgmot_fsc1
-
-python train.py --config configs/plgmot_fsc.py --work-dir ./work_dirs/plgmot_fsc2
-python detect.py --exp_id plgmot_fsc2 --config work_dirs/plgmot_fsc2/plgmot_fsc.py --checkpoint work_dirs/plgmot_fsc2/latest.pth
-python hmaa_track.py --exp_id plgmot_fsc2
-python sort.py --exp_id plgmot_fsc2
